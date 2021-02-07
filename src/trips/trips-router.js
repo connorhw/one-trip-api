d52375eb4f54af1cd3e199918b44787464cf54ce
connorhw <connorwong93@gmail.com>
2021-02-07 01:51:05 -0500

    adds post functionality to trips_table in trips db

--- src/trips/trips-router.js
+++ src/trips/trips-router.js
@@ -7,6 +7,14 @@ const {serializeTrips} = require('./trips-service')
 const tripsRouter = express.Router()
 const jsonParser = express.json()
 
+serializeTrip = trip => ({
+    id: trip.id,
+    trip_name: trip.trip_name,
+    places: trip.places,
+    fav_part: trip.fav_part,
+    journal: trip.journal,
+})
+
 tripsRouter
     .route('/')
     .get((req, res, next) => {
@@ -18,5 +26,29 @@ tripsRouter
             })
             .catch(next)
     })
+    .post(jsonParser, (req, res, next) => {
+        const { trip_name, places, fav_part, journal } = req.body
+        const newTrip = { trip_name, places, fav_part, journal }
 
-    module.exports = tripsRouter
+        for(const [key, value] of Object.entries(newTrip))
+            if (value == null)
+                return res.status(400).json({
+                    error: { message: `You are missing '${key}' in request body` }
+                })
+        TripsService.insertTrip(
+            req.app.get('db'),
+            newTrip
+        )
+            .then(trip => {
+                res
+                    .status(201)
+                    .location(path.posix.join(req.originalUrl, `/${trip.id}`))
+                    .json(serializeTrip(trip))
+            })
+            .catch(next)
+    })
+
+tripsRouter
+
+
+module.exports = tripsRouter
--- src/trips/trips-service.js
+++ src/trips/trips-service.js
@@ -4,6 +4,32 @@ const TripsService = {
     getAllTrips(knex) {
         return knex.select('*').from('trips_table')
     },
+    getById(knex, id) {
+        return knex
+            .from('trips_table')
+            .select('*')
+            .where('id', id)
+            .first()
+    },
+    insertTrip(knex, newTrip) {
+        return knex
+            .insert(newTrip)
+            .into('trips_table')
+            .returning('*')
+            .then(rows => {
+                return rows[0]
+            })
+    },
+    deleteTrip(knex, id) {
+        return knex('trips_table')
+            .where({ id })
+            .delete()
+    },
+    updateTrip(knex, id, newTripFields) {
+        return knex('trips_table')
+            .where({ id })
+            .update(newTripFields)
+    },
     serializeTrips(trip) {
         return {
             id: trip.id,
