bd84fcc6d2da5a3a97d604a4f9cc7257e7ab5729
connorhw <connorwong93@gmail.com>
2021-02-11 00:39:19 -0500

    adds migrations scripts for create trips users table. Still need to run these migrations and seed file.

--- seeds/seed.trips_table.sql
+++ seeds/seed.trips_table.sql
@@ -1,7 +1,8 @@
 BEGIN;
 
 TRUNCATE
-    trips_table
+    trips_table,
+    trips_users,
     RESTART IDENTITY CASCADE;
     
 INSERT INTO trips_table (trip_name, places, fav_part, total_cost, journal)
@@ -14,5 +15,13 @@ VALUES
     ('seattle','1. efefreferfe 2. fddvfdvfdvf 3. fvdfvfvfdvfdvfdv', 'seafood', 2200, 'Day1: kfvmdfjvfjvnfjdv Day2: smckfdmvkdfmvkfdmv Day3: vndfjvndfjvndfjvnf'),
     ('vancouver','1. efefreferfe 2. fddvfdvfdvf 3. fvdfvfvfdvfdvfdv', 'atmosphere', 2000, 'Day1: kfvmdfjvfjvnfjdv Day2: smckfdmvkdfmvkfdmv Day3: vndfjvndfjvndfjvnf'),
     ('havasupai','1. efefreferfe 2. fddvfdvfdvf 3. fvdfvfvfdvfdvfdv', 'hikes', 800, 'Day1: kfvmdfjvfjvnfjdv Day2: smckfdmvkdfmvkfdmv Day3: vndfjvndfjvndfjvnf');
+
+INSERT INTO trips_users (first_name, last_name, email, user_name, password)
+VALUES
+    ('Connor', 'Wong', 'connorwong@gmail.com', 'connorhw', 'password1'),
+    ('Ellen', 'Fung', 'ellenfung@yahoo.com', 'mellen', 'password2'),
+    ('Luke', 'Chiang', 'lukechiang@comcast.net', 'lukey', 'password3'),
+    ('Holly', 'Lee', 'hollylee@hotmail.com', 'hollytamalee', 'password4'),
+    ('Pickles', 'n/a', 'lilmisspickles@ig.com', 'lilmisspickles', 'password5');
 
 COMMIT;
