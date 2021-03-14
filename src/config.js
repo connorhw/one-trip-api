0584e143172236f010c8eb67369a733642cfa50d
connorhw <connorwong93@gmail.com>
2021-03-13 23:51:10 -0500

    cors still

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    //PORT: process.env.PORT || 8000,
-    PORT: 1337,
+    PORT: process.env.PORT || 1338,
+    //PORT: 1337,
     NODE_ENV: process.env.NODE_ENV || 'development',
     CLIENT_ORIGIN: 'http://localhost:3000',
     DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/one-trips',
